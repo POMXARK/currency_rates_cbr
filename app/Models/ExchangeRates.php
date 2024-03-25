05e937dab8b48576c854279a0fc83fa94ba77623
Роман Бушуев <bushuev@benefitty.ru>
2024-03-25 14:08:06 +0400

    build: install laravel-mongodb
    Инсталляция laravel-mongodb.

--- app/Models/ExchangeRates.php
+++ app/Models/ExchangeRates.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
 
 class ExchangeRates extends Model
 {
     use HasFactory;
+
+    protected $connection = 'mongodb';
 }
