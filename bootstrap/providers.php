baefda40747b3e94bfc360c1a529236833766ac7
Роман Бушуев <bushuev@benefitty.ru>
2024-03-26 11:27:59 +0400

    Schedule планировщик задач, horizon админ панель просмотра заданий в очереди

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     MongoDB\Laravel\MongoDBServiceProvider::class,
 ];
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Jobs\ExchangeRatesJob;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::job(new ExchangeRatesJob)
+    ->skip(function () {
+        return Cache::get('exchange_rates');
+    })
+    ->daily()
+    ->withoutOverlapping();
